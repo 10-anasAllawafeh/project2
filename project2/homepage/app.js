add837aea7da4b5a03db6f82aac4c089fdb87bf0
10-Anas <anasq0q@gmail.com>
2022-04-01 07:40:07 -0700

    try

--- project2/homepage/app.js
+++ project2/homepage/app.js
@@ -20,17 +20,55 @@ let userinfo=[];
 // validation(name,mail,password);
     
 // })
+function transpert() {
+    document.querySelector(input).style.display="none";
+    document.querySelector("subdive").style.display="block";
+
+}
 
 function validation(userName,registerEmail,registerPass,registerConfPass) {
-    
+     userName=document.querySelector("userName");
+     registerEmail=document.querySelector("registerEmail");
+     registerPass=document.querySelector("registerPass");
+     registerConfPass=document.querySelector("registerConfPass");
+
+
     let pwd_expression = /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
     let letters = /[A-Za-z]+$/;
     let filter = /([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     let capital=/[A_Z]/;
 
-if (!letters.test(userName)) {
-    alert("User name should only have lphapatic letters")
-}
+    // if(userName=='')
+    
+    //     alert('Please enter your first userName');
+    
+    // else if(!letters.test(userName))
+    
+    //     alert('First name field required only alphabet characters');
+    
+    
+    // else if(registerEmail=='')
+    
+    //     alert('Please enter your user registerEmail id');
+    
+    // else if (!filter.test(registerEmail))
+    
+    //     alert('Invalid registerEmail');
+    
+    // else if(registerPass=='')
+    
+    //     alert('Please enter Password');
+    
+    // else if(!pwd_expression.test(registerPass) && !capital.test(pwd[0]))
+
+    //     alert('Password should start with capital letter and indicate letters,numbers,symbols');
+
+    
+    // else if(registerConfPass === registerPass)
+    
+    //     alert('Password dont match');
+    
+        transpert()
 }
 
 
